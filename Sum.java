0f1912967be7724d4c80b74b9e7b604bdb0c25c3
ISHUNAGENDRAN <ishunagendran96@gmail.com>
2017-06-03 12:31:50 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -7,10 +7,15 @@ public class Sum
     int sum=0;
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
+        if(n>0)
+        {
         for(int i=0;i<n;i++)
         {
         sum=sum+i;
         }
         System.out.println(sum);
+        }
+        else if((n==0)||(n<0))
+            System.out.println("invalid");
         }
         }
