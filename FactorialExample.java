eeb940be5dd6bdbb8bd52afb274d9c87625dc96d
ISHUNAGENDRAN <ishunagendran96@gmail.com>
2017-06-05 18:39:42 +0530

    Update and rename Factorial.java to FactorialExample.java

---
+++ FactorialExample.java
@@ -0,0 +1,10 @@
+    import java.util.*;
+    class FactorialExample{
+    public static void main(String args[]){
+    int i,fact=1;
+    Scanner sc=new Scanner(System.in);
+    int number=sc.nextInt();
+    for(i=1;i<=number;i++){
+    fact=fact*i;
+    }
+    System.out.println("Factorial of "+number+" is: "+fact);
