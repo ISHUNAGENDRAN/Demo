4e50e1d99d223fa8026dd822f7d0e1b2b14532ab
ISHUNAGENDRAN <ishunagendran96@gmail.com>
2017-06-05 18:56:15 +0530

    Update and rename Prime.java to PrimeRange.java

---
+++ PrimeRange.java
@@ -0,0 +1,45 @@
+  import java.util.*;
+   public class PrimeRange {
+
+	    public static void main(String[] args) {
+
+	        Scanner sc=new Scanner(System.in);
+	        long starting_number=sc.nextInt();
+	         long ending_number=sc.nextInt();
+	        long totals = 0;
+
+	        System.out.println("List of prime numbers between " + starting_number + " and " + ending_number);
+
+	 
+
+	        for (long current = starting_number; current <= ending_number; current++) {
+
+	            long sqr_root = (long) Math.sqrt(current);
+
+	            boolean is_prime = true;
+
+	            for (long i = 2; i <= sqr_root; i++) {
+
+	                if (current % i == 0) {
+
+	                    is_prime = false; // Current is not prime.
+
+	                }
+
+	            }
+
+	            if (is_prime) {
+
+	                System.out.println(current);
+
+	                totals++;
+
+	            }
+
+	        }
+
+	        System.out.println("There are a total of "+totals+" prime numbers between "+starting_number+" and "+ending_number);
+
+	    }
+
+	}
