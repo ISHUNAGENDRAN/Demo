33bbe677e9de9f9cd28782a86e8d833b41ded2b5
ISHUNAGENDRAN <ishunagendran96@gmail.com>
2017-06-14 09:47:15 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class num{
+
+    public static void main(String[] args) {
+
+       Scanner in=new Scanner(System.in);
+ int i=0,flag=0,count=0;
+ int n=in.nextInt();
+ int k=in.nextInt();
+ for(i=n-k+1;i<=n;i++)
+ {
+     System.out.print(i);
+ }
+ for(i=1;i<n-k+1;i++)
+ {
+     System.out.print(i);
+ }
+     
+     
+ 
+    }
+}
