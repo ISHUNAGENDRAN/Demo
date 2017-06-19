c7093a058e332499d5dc3a863b07b68824139a97
ISHUNAGENDRAN <ishunagendran96@gmail.com>
2017-06-19 21:21:09 +0530

    Create holidayswrk.java

---
+++ holidayswrk.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class holidayswrk
+{
+public static  void main(String args[])
+{
+if(args[0]=="sunday"|| args[0]=="saturday")
+System.out.print(false);
+else
+System.out.print(true);
+}
+}
