1adba05ea96ed676c40d2f5a0c2120fb39be83a1
ISHUNAGENDRAN <ishunagendran96@gmail.com>
2017-07-06 16:00:20 +0530

    Create LargestPrefixinstring.java

---
+++ LargestPrefixinstring.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class LargestPrefixinstring
+{
+    public static void main(String a[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String arr[]=str.split(" ");
+        String largestPre="";
+        String s1=arr[0];
+        for(int i=0;i<s1.length();i++)
+        {
+            int count=0;
+            for(int j=0;j<arr.length;j++)
+            {
+                String s2=arr[j];
+                if(i<s2.length())
+                if(s1.charAt(i)==s2.charAt(i))
+                    count++;
+                else
+                    break;
+            }
+            if(count==arr.length)
+            largestPre=largestPre+s1.charAt(i);
+        }
+        System.out.println(largestPre);
+    }
+}
